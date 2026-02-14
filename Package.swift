86fa9a0c542b94b0813c27770f1656ff4ba0c385
Aldo Becerril <aldobecerril@google.com>
2026-02-13 17:01:04 -0800

    Adds GoogleMobileAds as a dependency
    PiperOrigin-RevId: 869944457

--- Package.swift
+++ Package.swift
@@ -25,11 +25,18 @@ let package = Package(
       targets: ["UnityAdapterTarget", "UnityAds"]
     )
   ],
+  dependencies: [
+    .package(
+      url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
+      from: "12.0.0"
+    )
+  ],
   targets: [
     .target(
       name: "UnityAdapterTarget",
       dependencies: [
         .target(name: "UnityAdapter"),
+        .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
       ],
       path: "UnityAdapterTarget"
     ),
