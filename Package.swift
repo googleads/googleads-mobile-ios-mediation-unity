e7be9bf6facd8c5d1cec14bddf91273b5c433a63
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2026-05-14 13:16:41 -0700

    Updating Unity Ads iOS adapter for release version 4.18.0.0
    PiperOrigin-RevId: 915581661

--- Package.swift
+++ Package.swift
@@ -44,13 +44,13 @@ let package = Package(
       name: "UnityAdapter",
       url:
         "https://dl.google.com/googleadmobadssdk/mediation/ios/unity/UnityAdapter-4.18.0.0.zip",
-      checksum: "fd66c28fc4195377c0fd7a98c422305548931df7f9127c3df320452a5bfd8f74"
+      checksum: "06982d0b1bb3fdf39864b6164a8de7a9d6d3917e0b9e25fbaf8853705387e543"
     ),
     .binaryTarget(
       name: "UnityAds",
       url:
         "https://github.com/Unity-Technologies/unity-ads-ios/releases/download/4.18.0/UnityAds.zip",
-      checksum: "8a40e6c779353e4ead87d4d635480cdae46f0b708763460dc002103e35ce8ba7"
+      checksum: "e027a0ab66f7d57a7e59855c20d7793b30a5a25b1f1339412970c8a2ef932b6f"
     ),
   ]
 )
